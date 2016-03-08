fce32521415d2778aa57b35365033eb035a26a83
Szymon Iwacz <szymon@iwacz.pl>
2016-03-08 15:54:01 +0100

    Comment unused code.

--- lib/toy_robot_simulator/surface.rb
+++ lib/toy_robot_simulator/surface.rb
@@ -9,17 +9,17 @@ class Surface
     x >= 0 && y >= 0 && @surface[x,y] == 0 ? true : false
   end
 
-  def visualize
-    @surface.to_a.each do |row|
-      print "#{row.join(" ")} \n"
-    end
-  end
-
-  def visualize(robot)
-    x,y = robot.x_pose, robot.y_pose
-    @surface.send(:[]=, x, y, "x")
-    @surface.rotate(2).to_a.reverse.each do |row|
-      print "#{row.join(" ")} \n"
-    end
-  end
+  # def visualize
+  #   @surface.to_a.each do |row|
+  #     print "#{row.join(" ")} \n"
+  #   end
+  # end
+  #
+  # def visualize(robot)
+  #   x,y = robot.x_pose, robot.y_pose
+  #   @surface.send(:[]=, x, y, "x")
+  #   @surface.rotate(2).to_a.reverse.each do |row|
+  #     print "#{row.join(" ")} \n"
+  #   end
+  # end
 end
